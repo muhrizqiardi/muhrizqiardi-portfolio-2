4ac9fd02c12676ab704397a7709ed57002723aea
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-07-20 11:32:24 +0700

    changed meta tags again

--- pages/_app.js
+++ pages/_app.js
@@ -25,13 +25,13 @@ function MyApp({ Component, pageProps }) {
         <meta property="og:url" content="https://muhrizqiardi.github.io" />
         <meta property="og:title" content="Muhammad Rizqi Ardiansyah | Portfolio Website" />
         <meta property="og:description" content="Hi, I'm Muhammad Rizqi Ardiansyah, a web developer looking for a partnership opportunity and an Informatics Engineering. Passionate and curious about technology!" />
-        <meta property="og:image" content="/assets/portfolio-thumbnail.png" />
+        <meta property="og:image" content="https://muhrizqiardi.github.io/assets/portfolio-thumbnail.png" />
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://muhrizqiardi.github.io" />
         <meta property="twitter:title" content="Muhammad Rizqi Ardiansyah | Portfolio Website" />
         <meta property="twitter:description" content="Hi, I'm Muhammad Rizqi Ardiansyah, a web developer looking for a partnership opportunity and an Informatics Engineering. Passionate and curious about technology!" />
-        <meta property="twitter:image" content="/assets/portfolio-thumbnail.png" />
+        <meta property="twitter:image" content="https://muhrizqiardi.github.io/assets/portfolio-thumbnail.png" />
 
         {/* Fonts */}
         <link href="https://fonts.googleapis.com/css?family=Inter:100,200,300,regular,500,600,700,800,900" rel="stylesheet" />
