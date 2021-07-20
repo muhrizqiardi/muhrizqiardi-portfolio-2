3504299c62d2471bb82fa732d02331df1f984918
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-07-20 10:30:00 +0700

    removed exportPathMap

--- next.config.js
+++ next.config.js
@@ -12,10 +12,4 @@ module.exports = {
   },
 
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/muhrizqiardi/muhrizqiardi.github.io/github-pages/' : '',
-
-  exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    return {
-      '/': { page: '/en' }
-    }
-  }
 }
