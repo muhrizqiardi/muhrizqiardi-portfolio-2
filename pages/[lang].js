f9822f0426b8d74ca4cf2c92383fc94e3b42a6f8
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-07-20 10:26:37 +0700

    removed api

--- pages/[lang].js
+++ pages/[lang].js
@@ -8,7 +8,100 @@ import { useRouter } from 'next/router';
 
 
 export default function Home(props) {
-  const [projectsData, setProjectsData] = useState([]);
+  const projectsData = [
+    {
+      "title": "Notefield",
+      "body": {
+        "en": "A note-taking app made with React libary for the front-end, and Express for the API. I made this because I feel like all note-taking app in the market isn't good enough for me.",
+        "id": "Aplikasi catatan dibuat dengan library React serta Express untuk pembuatan API. Saya membuat ini karena saya merasa aplikasi catatan yang sudah ada di pasar belum mencukupi kebutuhan saya.",
+        "jp": "ReactとExpressで構築したメモ帳のアプリ。私がこのアプリを構築した理由は現在にあるメモ帳アプリが物足りないと思ったからです。"
+      },
+      "image": "/assets/projects-thumbnail/notefield.png",
+      "tags": [
+        "React",
+        "Sass",
+        "Express",
+        "MongoDB"
+      ],
+      "href": "https://notefield.vercel.app/"
+    },
+    {
+      "title": "react-todo",
+      "body": {
+        "en": "A to-do list app made with React library for the front-end, and Express for the API.",
+        "id": "Aplikasi to-do list dibuat menggunakan React dan Express.",
+        "jp": "ReactとExpressで構築したToDoリストアプリ"
+      },
+      "image": "/assets/projects-thumbnail/react-todo.png",
+      "tags": [
+        "React",
+        "Sass",
+        "Express",
+        "MongoDB"
+      ],
+      "href": "https://muhrizqiardi-react-todo.herokuapp.com/"
+    },
+    {
+      "title": "creative@home",
+      "body": {
+        "en": "A website my team created for a web design competition.",
+        "id": "Website yang dibuat berkolaborasi dengan tim untuk kompetisi web design.",
+        "jp": "私のチームがウェブデザインコンペのために制作したウェブサイトです。"
+      },
+      "image": "/assets/projects-thumbnail/creative-at-home.png",
+      "tags": [
+        "JavaScript"
+      ],
+      "href": "https://varcode-project.github.io/creative-at-home/"
+    },
+    {
+      "title": "Koffee Khayalaan Store",
+      "body": {
+        "en": "An e-commerce and a landing website for a fictional cafe. Made with Next.js for the front-end and Strapi as the headless CMS to provide an API to the front-end side.",
+        "id": "Website toko online serta landing website untuk kafe fiksional. Dibuat dengan Next.js serta Strapi CMS.",
+        "jp": "喫茶店のオンラインショップとランディングページ。Next.jsとStrapi CMSを使って制作しました。"
+      },
+      "image": "/assets/projects-thumbnail/Koffee-Khayalaan.png",
+      "tags": [
+        "React",
+        "Next.js",
+        "Sass",
+        "Strapi"
+      ],
+      "href": "https://koffee-khayalaan.vercel.app/"
+    },
+    {
+      "title": "muhrizqiardi.github.io",
+      "body": {
+        "en": "This (beautiful) website.",
+        "id": "Website ini.",
+        "jp": "現在のウェブサイトです。"
+      },
+      "image": "/assets/projects-thumbnail/muhrizqiardi.github.io.png",
+      "tags": [
+        "React",
+        "Next.js",
+        "Tailwind CSS"
+      ],
+      "href": "https://muhrizqiardi.github.io/"
+    },
+    {
+      "title": "My Blog",
+      "body": {
+        "en": "The place where I share my thougts.",
+        "id": "Tempat saya membagikan opini dan pikiran saya.",
+        "jp": "私のブログサイトです。"
+      },
+      "image": "/assets/projects-thumbnail/my-blog.png",
+      "tags": [
+        "React",
+        "Next.js",
+        "Sass",
+        "Strapi"
+      ],
+      "href": "https://muhrizqiardi-blog.vercel.app/"
+    }
+  ];
   const [scrollState, setScrollState] = useState(false);
   const [mobileState, setMobileState] = useState(false);
   const [navState, setNavState] = useState(false);
@@ -20,15 +113,6 @@ export default function Home(props) {
   };
   const handleNav = () => {
     setNavState(x => !x);
-  }
-  const fetchProjects = () => {
-    axios.get('/api/projects')
-      .then(function (response) {
-        setProjectsData(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
   }
 
   useEffect(() => {
@@ -47,17 +131,15 @@ export default function Home(props) {
   });
 
   useEffect(() => {
-    console.log(router)
     Aos.init({
       offset: 0
     })
-    fetchProjects();
   }, []);
 
 
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popper' : undefined;
-
+  
   return (
     <>
       <header id="header" className={`px-5 md:px-20 sticky top-0 z-40 ${scrollState ? " bg-light shadow-md" : ""}`}>
