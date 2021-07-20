21ac1f5a9738e7a10e63d42541936299fa6c8523
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-07-20 10:50:42 +0700

    removed nextjs' redirect

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/en',
-        permanent: true,
-      },
-    ]
-  },
-
   assetPrefix: isProd ? 'https://cdn.statically.io/gh/muhrizqiardi/muhrizqiardi.github.io/github-pages/' : '',
 }
