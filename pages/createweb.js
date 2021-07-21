d3c1e6f96add1dc68bd67d3d4fac08c843475dea
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-07-21 19:33:53 +0700

    Added /createweb page, added sitemap

---
+++ pages/createweb.js
@@ -0,0 +1,11 @@
+function CreateWebPage(props) {
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center text-center">
+      <p>Work In Progress.</p> 
+      <p>You can contact me on <a className=" text-blue-700 hover:underline" href="mailto:muhrizqiardi@gmail.com">muhrizqiardi@gmail.com</a>, if you are interested.</p>
+      <p>Thank you.</p>
+      <a className="my-4 text-blue-700 hover:underline"  href="/">Go Back to Homepage.</a>
+    </div>
+  );
+}
+export default CreateWebPage;
