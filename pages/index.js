639262f9d2cefa2b7cf5d61b521a624365917fd5
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-07-21 19:37:23 +0700

    Fixed route

--- pages/index.js
+++ pages/index.js
@@ -184,7 +184,7 @@ export default function Home(props) {
               </a>
             }
             {langState === 'en' &&
-              <a onClick={handleNav} className="px-3 py-2 hover:bg-gray-300 rounded-md" href={`/createweb`}>
+              <a onClick={handleNav} className="px-3 py-2 hover:bg-gray-300 rounded-md" href={`/web-services`}>
                 Create a Website{' '}
                 <i className='bx bx-link-external' ></i>
               </a>
