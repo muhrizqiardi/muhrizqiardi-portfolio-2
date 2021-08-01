95125b24c4666241bc88b896d721bf24d05c0f36
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-08-01 13:54:43 +0700

    removed next config (it's no longer needed)

--- next.config.js
+++
@@ -1,5 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/muhrizqiardi/muhrizqiardi.github.io/github-pages/' : '',
-}
